3f7530fe80cf425aae86aee4dcfe2758eee69b2d
Zack Ward <wardzb7@gmail.com>
2021-11-03 18:54:39 -0400

    Update seed file with some Halloween picks

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,7 +1,37 @@
-const { db } = require("./index");
+const { db, Movie, Genre } = require("./index");
 
 const runSeed = async() => {
   await db.sync({ force: true });
+
+  const horrorGenre = await Genre.create({ name: "Horror" });
+  const ComedyGenre = await Genre.create({ name: "Comedy" });
+  const sciFiGenre = await Genre.create({ name: "Science Fiction" });
+  const dramaGenre = await Genre.create({ name: "Drama" });
+  const animGenre = await Genre.create({ name: "Animation" });
+  const familyGenre = await Genre.create({ name: "Family" });
+
+  const donnieDarko = await Movie.create({
+    title: "Donnie Darko",
+    imdbLink: "https://www.imdb.com/title/tt0246578/"
+   });
+   await donnieDarko.setGenres([horrorGenre, sciFiGenre]);
+
+   const cabinInTheWoods = await Movie.create({
+     title: "Cabin in the Woods"
+   });
+   await cabinInTheWoods.setGenres([horrorGenre, ComedyGenre, sciFiGenre]);
+
+   const corpseBride = await Movie.create({
+     title: "Corpse Bride",
+     imdbLink: "https://www.imdb.com/title/tt0121164/"
+   });
+   await corpseBride.setGenres([animGenre, dramaGenre, familyGenre]);
+
+   const nightmareBeforeChristmas = await Movie.create({
+     title: "Nightmare Before Christmas",
+   });
+   await nightmareBeforeChristmas.setGenres([familyGenre, animGenre])
+
   console.log("Database seeded!");
   process.kill(0);
 };
